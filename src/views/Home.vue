29e81dad8bd020196629f50bc98cc2c40ea93f6b
mz-2 <837084459@qq.com>
2020-03-30 16:49:12 +0800

    提交 备忘

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+      配色学习
+      http://v.bootstrapmb.com/2019/10/plij06445/pages/dashboards/dashboard.html
+      http://www.bootstrapmb.com/tag/houtaixitong
   </div>
 </template>
 
